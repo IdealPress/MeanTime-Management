df6cd5fe5dd338d9c9bfc42e874de52502c9e4bf
Jack Clarke <hello@idealpress.org>
2022-11-11 15:49:58 +0000

    Add typeface

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,25 +4,28 @@ import "./Hero.css"
 // Assets
 import hero from "../assets/greenwich.jpeg"
 import arrow from "../assets/Arrow.svg"
+import { Link } from "react-router-dom"
 
 export default function Hero () {
   return (
     <section>
-      <div 
-        className="hero"
-        style={{ backgroundImage: `url(${hero})` }} 
-      >
-        <div>
-          <div className="left">
-            <span>MeanTimes Newsletter</span>
-            <h2>Flash Crash!</h2>
-          </div>
-          <div className="right">
-            <img src={arrow} />
-            <span>#1 — 14/10/22</span>
+      <Link to="/timeline">
+        <div 
+          className="hero"
+          style={{ backgroundImage: `url(${hero})` }} 
+        >
+          <div>
+            <div className="left">
+              <span>MeanTimes Newsletter</span>
+              <h2>Flash Crash!</h2>
+            </div>
+            <div className="right">
+              <img src={arrow} />
+              <span>#1 — 14/10/22</span>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </section>
   )
 }
